cd3606f1a5968cfe00d9e579055b31eda659e46c
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-05-05 20:11:17 +0400

    Create login form and implement login and logout

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
-include_once '../Request.php';
-include_once '../Router.php';
+require_once __DIR__ . '/../helpers.php';
+require_once __DIR__ . '/../Request.php';
+require_once __DIR__ . '/../Router.php';
+require_once __DIR__ . '/../db/Database.php';
 
+session_start();
+
+$db = new Database();
 
 $router = new Router(new Request);
 
@@ -11,6 +16,20 @@ $router->get('/about', function ($request) {
     return <<<HTML
   <h1>about</h1>
 HTML;
+});
+$router->get('/login', 'login');
+$router->get('/logout', function(){
+    session_unset();
+    session_destroy();
+    redirect('/');
+});
+$router->post('/submit-login', function (IRequest $request) use ($router, $db) {
+    $body = $request->getBody();
+    if ($db->loginUser($body['email'], $body['password'])) {
+        redirect('/');
+    } else {
+        redirect('/login');
+    }
 });
 $router->post('/data', function ($request) {
     return json_encode($request->getBody());
---
+++ views/login.php
@@ -0,0 +1,15 @@
+<div>
+    <h1>Login</h1>
+    <form method="post" action="/submit-login">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" name="email"
+                   placeholder="Enter email">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
