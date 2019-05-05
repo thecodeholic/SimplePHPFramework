471ff7d3d9dcaa5a8ba209c648810c6065c32967
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-05-05 18:51:23 +0400

    Create config.php and config.sample.php to store database configuration

---
+++ config.sample.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User: TheCodeholic
+ * Date: 5/5/2019
+ * Time: 6:26 PM
+ */
+return [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'custom_framework'
+];
