17f33e0eb665c10748f510872e05a5758b33433a
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-05-05 20:08:59 +0400

    Fix bug in Request::getBody method

--- Request.php
+++ Request.php
@@ -35,7 +35,7 @@ class Request implements IRequest
         if ($this->requestMethod === "POST") {
             $body = array();
             foreach ($_POST as $key => $value) {
-                $body[$key] = filter_input(INPUT_POST, $value, FILTER_SANITIZE_SPECIAL_CHARS);
+                $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
             }
             return $body;
         }
