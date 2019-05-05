4b435a1b33fa148458fd58864ac9c41e0caf4b42
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-05-05 18:51:51 +0400

    Create Database.php and migrations file

---
+++ db/Database.php
@@ -0,0 +1,36 @@
+<?php
+
+class Database
+{
+    private $servername;
+    private $username;
+    private $password;
+    private $database;
+
+    private $connection;
+
+    public function __construct()
+    {
+        $config = require __DIR__ . '/../config.php';
+        $this->servername = $config['host'];
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->database = $config['database'];
+        $this->connection = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
+        // set the PDO error mode to exception
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    public static function loginUser($username, $password)
+    {
+
+    }
+}
---
+++ db/migrations.php
@@ -0,0 +1,31 @@
+<?php
+
+$config = require __DIR__ . '/../config.php';
+
+$servername = $config['host'];
+$username = $config['username'];
+$password = $config['password'];
+$database = $config['database'];
+
+$conn = new PDO("mysql:host=$servername", $username, $password);
+
+try {
+    $sql = "CREATE DATABASE $database";
+    $conn->exec($sql);
+    echo "Database created successfully" . PHP_EOL;
+    $conn->query("use $database");
+
+    $sql = "CREATE TABLE users (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        username VARCHAR(30) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255),
+        reg_date TIMESTAMP
+    )";
+
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Table \"users\" created successfully" . PHP_EOL;
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
